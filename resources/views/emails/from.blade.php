f709bf6d3da01b3d2ce3a5a0feaf8938bc15cba0
H37kouya <46477357+H37kouya@users.noreply.github.com>
2020-01-05 14:07:28 +0900

    [Update]: @isset is used.

--- resources/views/emails/from.blade.php
+++ resources/views/emails/from.blade.php
@@ -1,22 +1,22 @@
 送信された場所：{{ $content['place'] }}
 お名前：{{ $content['from_name'] }}
 メールアドレス：{{ $content['from'] }}
-@if(isset($content['type']))
+@isset($content['type'])
 お問い合わせ種類：{{ $content['type'] }}
-@endif
+@endisset
 @isset($content['url'])
 送信されたURL: {{ $content['url'] }}
 @endisset
-@if(isset($content['tel']))
+@isset($content['tel'])
 電話番号：{{ $content['tel'] }}
-@endif
-@if(isset($content['postal']))
+@endisset
+@isset($content['postal'])
 郵便番号：{{ $content['postal'] }}
-@endif
-@if(isset($content['address']))
+@endisset
+@isset($content['address'])
 住所：{{ $content['address'] }}
-@endif
-@if(isset($content['body']))
+@endisset
+@isset($content['body'])
 お問い合わせ内容
 {{ $content['body'] }}
-@endif
+@endisset
